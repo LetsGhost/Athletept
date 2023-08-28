c3c575ce2a19911843812860f3f1493fe7b9fa0b
LetsGhost <69594831+LetsGhost@users.noreply.github.com>
2023-08-28 21:02:41 +0200

    Removed .ts

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,27 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
-import userRoutes from "./app/routes/UserRoutes.ts";
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
-import { connectToDatabase, } from "./config/db.ts";
+import { connectToDatabase, } from "./config/db";
+import userRoutes from "./app/routes/UserRoutes";
+import authRoutes from "./app/routes/AuthRoute";
 
 const server = express();
 
 server.use(bodyParser.json());
+server.use(cookieParser());
 
 connectToDatabase()
 
 server.use('/user', userRoutes);
+server.use('/auth', authRoutes);
 
 server.get('/', (req, res) => {
     res.send('Hello, TypeScript Express!');
 });
 
 server.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+    console.log(`Server is running on port ${process.env.PORT}`)
 });
