fc7fcd1ccd2201a975900497446c0f0605f69b21
LetsGhost <69594831+LetsGhost@users.noreply.github.com>
2023-08-28 21:04:13 +0200

    Added data to user schema

--- src/app/models/UserModel.ts
+++ src/app/models/UserModel.ts
@@ -1,17 +1,44 @@
-import { Schema, model, Document } from 'mongoose';
+import mongoose, { Schema, model, Document } from 'mongoose';
 import { v4 as uuidv4 } from 'uuid';
 import bcrypt from 'bcrypt';
 
-interface User extends Document {
+export interface User extends Document {
     uuid: string;
     email: string;
     password: string;
+    role: 'admin' | 'user';
+    userInfo: {}
+    exercisePlan: mongoose.Schema.Types.ObjectId;
+    warmup: mongoose.Schema.Types.ObjectId;
+    messages: mongoose.Schema.Types.ObjectId[];
+    checkIn: mongoose.Schema.Types.ObjectId;
+    weekDisplay: mongoose.Schema.Types.ObjectId;
+    comparePassword(candidatePassword: string): Promise<boolean>;
 }
 
 const userSchema = new Schema<User>({
     uuid: { type: String, default: uuidv4, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    userInfo: {},
+    role: { type: String, enum: ['admin', 'user'], default: 'user' },
+    exercisePlan: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'ExercisePlan'
+    },
+    warmup: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Warmup'
+    },
+    messages: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Message'
+    }],
+    checkIn: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'CheckIn'
+    },
+    weekDisplay: {}
 });
 
 // Hash the password before saving
