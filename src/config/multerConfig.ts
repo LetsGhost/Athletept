45d0823c3f8a8c7f126f179ee6e59cf0fbb12548
LetsGhost <69594831+LetsGhost@users.noreply.github.com>
2023-09-02 17:27:44 +0200

    Multer config

---
+++ src/config/multerConfig.ts
@@ -0,0 +1,13 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, '../public/uploads'); // Uploads will be stored in the 'uploads' directory
+    },
+    filename: (req, file, cb) => {
+        const fileName = `${Date.now()}-${file.originalname}`;
+        cb(null, fileName);
+    },
+});
+
+export const upload = multer({ storage: storage });
