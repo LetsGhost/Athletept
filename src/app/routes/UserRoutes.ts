4971106c8d6c286f5c9de1b804917556d6953f6e
LetsGhost <69594831+LetsGhost@users.noreply.github.com>
2023-09-10 21:32:07 +0200

    Create Protocol and get Protocol

--- src/app/routes/UserRoutes.ts
+++ src/app/routes/UserRoutes.ts
@@ -1,9 +1,11 @@
 // src/routes/userRoutes.ts
 import express from 'express';
+
 import { authenticateToken } from '../middleware/AuthenticateToken';
 import {getExercisePlanController} from "../controllers/ExercisePlanController";
 import {getUserByIdController} from "../controllers/UserController";
 import {getAllMessagesFromUser} from "../services/MessageService";
+import {createProtocolController} from "../controllers/ProtocolController";
 
 const router = express.Router();
 
@@ -13,4 +15,5 @@ router.get("/getUser/:userId", getUserByIdController)
 
 router.get("/getAllMessages/:userId", getAllMessagesFromUser)
 
+router.post("/createProtocol", createProtocolController)
 export default router;
