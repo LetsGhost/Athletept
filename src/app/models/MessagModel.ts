9ca491e0441f94b0a74685bcab92f9a6440256a9
Paul Ehrling <ghostlest330@gmail.com>
2023-11-06 10:11:33 +0100

    Install moment

--- src/app/models/MessagModel.ts
+++ src/app/models/MessagModel.ts
@@ -1,15 +1,22 @@
+import { timeStamp } from "console";
 import mongoose, {Schema, Document} from "mongoose";
+import moment from "moment";
 
 export interface Message extends Document {
     sendDate: Date;
     message: string;
     sender: string;
+    formattedDate?: string;
 }
 
 const messageSchema = new Schema<Message>({
     sendDate: { type: Date, required: true, default: Date.now },
     message: { type: String, required: true },
     sender: { type: String, required: true }
+}, { toJSON: { virtuals: true } });
+
+messageSchema.virtual('formattedDate').get(function(this: Message) {
+    return moment(this.sendDate).format('DD-MM-YYYY HH:mm:ss');
 });
 
 export const MessageModel = mongoose.model<Message>('Message', messageSchema);
