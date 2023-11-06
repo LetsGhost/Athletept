ceb9a26a5968c1b2a13ff622371955d6b9187a10
Paul Ehrling <ghostlest330@gmail.com>
2023-11-06 10:15:55 +0100

    Test Virtuals for Formated Dates, Add Commands

--- src/app/models/MessagModel.ts
+++ src/app/models/MessagModel.ts
@@ -13,8 +13,9 @@ const messageSchema = new Schema<Message>({
     sendDate: { type: Date, required: true, default: Date.now },
     message: { type: String, required: true },
     sender: { type: String, required: true }
-}, { toJSON: { virtuals: true } });
+}, { toJSON: { virtuals: true } }); // Stores the virtuals in the db
 
+// A Virtual is a property that is not stored in MongoDB just in Memory but can be used as a normal property and return the sendDate property as formatedDate
 messageSchema.virtual('formattedDate').get(function(this: Message) {
     return moment(this.sendDate).format('DD-MM-YYYY HH:mm:ss');
 });
