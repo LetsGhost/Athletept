0881840f08c2c1bddcb4d11289c3ce379128be64
Paul Ehrling <ghostlest330@gmail.com>
2023-11-17 11:53:22 +0100

    Also add get

--- src/app/controllers/WeightAnalyticsController.ts
+++ src/app/controllers/WeightAnalyticsController.ts
@@ -9,6 +9,14 @@ class WeightAnalyticsController {
 
         return res.status(code).json({ success, message });
     }
+
+    async getWeightAnalytics(req: Request, res: Response) {
+        const userId = req.params.userId;
+
+        const {success, code, message, weightAnalytics} = await WeightAnalyticsService.getWeightAnalytics(userId);
+
+        return res.status(code).json({ success, message, weightAnalytics });
+    }
 }
 
 export default new WeightAnalyticsController();
