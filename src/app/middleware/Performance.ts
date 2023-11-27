21c3443042f9b6b774a61a0ca20bc3962d014c45
Paul Ehrling <ghostlest330@gmail.com>
2023-11-27 11:30:24 +0100

    Interacts now with the redis

--- src/app/middleware/Performance.ts
+++ src/app/middleware/Performance.ts
@@ -1,11 +1,27 @@
 import { Request, Response, NextFunction } from 'express';
 import logger from '../../config/winstonLogger';
+import { redisClient } from '../../config/redis'; // import the Redis client from server.ts
 
 export function performanceLogger(req: Request, res: Response, next: NextFunction): void {
-    const start = Date.now();
-    res.on('finish', () => { 
-        const duration = Date.now() - start;
-        logger.info(`Request to ${req.path} took ${duration}ms`, {service: 'PerformanceLogger'});
-    });
-    next();
+  const start = Date.now();
+
+  res.on('finish', async () => { 
+    const duration = Date.now() - start;
+    const today = new Date().toISOString().slice(0, 10);
+    logger.info(`Request to ${req.path} took ${duration}ms`, {service: 'PerformanceLogger'});
+
+    if (redisClient) {
+      try {
+        // Store the request duration in Redis
+        const pathParts = req.path.split('/');
+        const key = `${pathParts[1]}:durations`; // Use only the first part of the path
+        await redisClient.lpush(key, `[${today}]Execution duration: ${duration}ms`);
+        await redisClient.ltrim(key, 0, 9); // Keep only the last 10 durations
+      } catch (err) {
+        logger.error(`Failed to store request duration in Redis: ${err}`, { service: 'PerformanceLogger' });
+      }
+    }
+  });
+
+  next();
 }
