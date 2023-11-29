52798dd23649d63d7bea307a13b29f62d6ff4a55
Paul Ehrling <ghostlest330@gmail.com>
2023-11-29 09:12:22 +0100

    Add log to rate limiter

--- src/app/middleware/Limiter.ts
+++ src/app/middleware/Limiter.ts
@@ -1,10 +1,17 @@
 import rateLimit from 'express-rate-limit';
+import logger from '../../config/winstonLogger';
+import { Request, Response, NextFunction } from 'express';
+import getClientIp from '../utils/ipUtils';
 
 // Define the rate limit rule
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
-  message: "Too many requests from this IP, please try again later."
+  handler: (req: Request, res: Response, next: NextFunction) => {
+    const ip = getClientIp(req);
+    logger.warn(`Rate limit exceeded by IP: ${ip}`, {service: 'Limiter'});
+    res.status(429).send("Too many requests from this IP, please try again later.");
+  }
 });
 
 export default limiter;
