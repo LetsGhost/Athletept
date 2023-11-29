7d35fdd09de6993fe38492374831d364d7a8a1cb
Paul Ehrling <ghostlest330@gmail.com>
2023-11-29 15:53:47 +0100

    Redis connection

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -5,11 +5,21 @@ let redisClient: Redis | null = null;
 
 const connectToRedis = async (): Promise<Redis | null> => {
   // redis://:password@localhost:6379
-  try {
-    const redisUrl = `process.env.REDIS_URL`;
+  try {    
+    const redisUrl = `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
+    console.log(`REDIS_URL: ${redisUrl}`); // Add this line
+    if (!redisUrl) {
+      logger.error('REDIS_URL is not defined', { service: 'connectToRedis' });
+      return null;
+    }
+
     logger.info(`Connecting to Redis at ${redisUrl}`, { service: 'connectToRedis' });
 
     redisClient = new Redis(redisUrl);
+
+    redisClient.on('error', (err) => {
+      console.error('Error connecting to Redis:', err);
+    });
   } catch (err) {
     logger.error(`Failed to connect or ping Redis: ${err}`, { service: 'connectToRedis' });
   }
