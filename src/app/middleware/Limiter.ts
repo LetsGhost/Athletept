62f3cd9e28b672e73bb97e56924f5c23dc35c1e5
Paul Ehrling <ghostlest330@gmail.com>
2023-12-21 18:53:22 +0100

    Update limiter settings in .env.example and Limiter.ts

--- src/app/middleware/Limiter.ts
+++ src/app/middleware/Limiter.ts
@@ -2,11 +2,13 @@ import rateLimit from 'express-rate-limit';
 import logger from '../../config/winstonLogger.js';
 import { Request, Response, NextFunction } from 'express';
 import getClientIp from '../utils/ipUtils.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 // Define the rate limit rule
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  windowMs: Number(process.env.LIMITER_WINDOW_MS) || 900000, // 15 minutes
+  max: Number(process.env.LIMITER_MAX) || 100, // limit each IP to 100 requests per windowMs
   handler: (req: Request, res: Response, next: NextFunction) => {
     const ip = getClientIp(req);
     logger.warn(`Rate limit exceeded by IP: ${ip}`, {service: 'Limiter'});
