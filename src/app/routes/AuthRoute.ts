e53dfde394f803c893ed388a1f5accd95f95ec33
Paul Ehrling <ghostlest330@gmail.com>
2024-02-05 10:01:15 +0100

    Add rate limiter middleware to login route

--- src/app/routes/AuthRoute.ts
+++ src/app/routes/AuthRoute.ts
@@ -3,10 +3,13 @@ import express from 'express';
 // Import Middlewares
 import auhtController from '../controllers/AuthController.js';
 
+// Import Rate Limiter
+import rateLimiter from '../middleware/Limiter.js';
+
 // Import routes
 const router = express.Router();
 
-router.post('/login', auhtController.login); // Is Documented
+router.post('/login',rateLimiter ,auhtController.login); // Is Documented
 router.get('/getUserFromToken', auhtController.getUserFromToken); // Is Documented
 
 export default router;
