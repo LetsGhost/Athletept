4e45e13c4ffc508a385f5864e58b39edc762c40c
Paul Ehrling <ghostlest330@gmail.com>
2024-04-27 19:05:16 +0200

    Add AuthMiddleware.test.ts to test authentication middleware

---
+++ src/tests/AuthenticateTests/AuthMiddleware.test.ts
@@ -0,0 +1,44 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import AuthService from '../../app/services/AuthService';
+
+describe('AuthMiddleware', () => {
+    it("should return a 200 code because everything is matching", async () => {
+        const userId = "123456"
+        const userRole = "user"
+
+        const token = jwt.sign({userId, userRole}, process.env.TOKEN_SECRET!, {expiresIn: '30s'});
+
+        const {success, code, message} = await AuthService.authToken(token, userId, "user/endpoint/123456")
+
+        expect(success).toBe(true);
+        expect(code).toBe(200);
+    })
+    it("should return a 401 code because of the diffrent userIds", async () => {
+        const userId = "123457"
+        const userIdToken = "123456"
+        const userRole = "user"
+
+        const token = jwt.sign({userIdToken, userRole}, process.env.TOKEN_SECRET!, {expiresIn: '30s'});
+
+        const {success, code, message} = await AuthService.authToken(token, userId, "user/endpoint/" + userId)
+
+        expect(success).toBe(false);
+        expect(code).toBe(401);
+        expect(message).toBe("Unauthorized userIds are not matching")
+    })
+    it("should return a 200 code because of the admin role", async () => {
+        const userId = "123457"
+        const userIdToken = "123456"
+        const userRole = "admin"
+
+        const token = jwt.sign({userIdToken, userRole}, process.env.TOKEN_SECRET!, {expiresIn: '30s'});
+
+        const {success, code, message} = await AuthService.authToken(token, userId, "user/endpoint/" + userId)
+
+        expect(success).toBe(true);
+        expect(code).toBe(200);
+    })
+})
