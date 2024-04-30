0909221bdc75f1c64ea79b8fb738dee8e323fdbf
Paul Ehrling <ghostlest330@gmail.com>
2024-04-30 16:43:47 +0200

    Add authentication tests for AuthMiddleware

--- src/tests/AuthenticateTests/AuthMiddleware.test.ts
+++ src/tests/AuthenticateTests/AuthMiddleware.test.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import AuthService from '../../app/services/AuthService';
+import exp from 'constants';
 
 describe('AuthMiddleware', () => {
     it("should return a 200 code because everything is matching", async () => {
@@ -40,5 +41,31 @@ describe('AuthMiddleware', () => {
 
         expect(success).toBe(true);
         expect(code).toBe(200);
+    })
+    it("should return a 401 code because of the diffrent roles", async () => {
+        const userId = "123457"
+        const userIdToken = "123456"
+        const userRole = "user"
+
+        const token = jwt.sign({userIdToken, userRole}, process.env.TOKEN_SECRET!, {expiresIn: '30s'});
+
+        const {success, code, message} = await AuthService.authRole(token, "admin/endpoint/" + userId)
+
+        expect(success).toBe(false);
+        expect(code).toBe(401);
+        expect(message).toBe("Unauthorized userRole is not matching")
+    })
+    it("should return a 200 code of the right role", async () => {
+        const userId = "123457"
+        const userIdToken = "123456"
+        const userRole = "admin"
+
+        const token = jwt.sign({userIdToken, userRole}, process.env.TOKEN_SECRET!, {expiresIn: '30s'});
+
+        const {success, code, message} = await AuthService.authRole(token, "admin/endpoint/" + userId)
+
+        expect(success).toBe(true);
+        expect(code).toBe(200);
+        expect(message).toBe("Authorized")
     })
 })
